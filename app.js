da92afa0ab07d585fc6d77a4b8756ee70e9adde7
ovaphlow <jsfreecss@163.com>
2016-12-24 15:14:06 +0800

    .

--- app.js
+++ app.js
@@ -12,8 +12,19 @@ app.get('/', function (req, res) {
   res.send('1123');
 });
 
-app.get('/test', function (req, res) {
-  res.send({message: 'OK'});
+app.get('/test', function (request, response) {
+  let data = {id: 1, nickname: '刘浪'}
+  response.send(data)
+})
+
+app.get('/test-list', function (req, res) {
+  let data = [
+    {id: 1, name: 'name1', title: 'title1'}
+    , {id: 2, name: 'name2', title: 'title2'}
+    , {id: 3, name: 'name3', title: 'title3'}
+    , {id: 4, name: 'name', title: 'title4'}
+  ]
+  res.send(data);
 });
 
 app.listen(80);
