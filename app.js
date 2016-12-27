e84f1992a145ec20313120083a256e56f0c749c2
ovaphlow <jsfreecss@163.com>
2016-12-27 16:33:57 +0800

    .

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ app.get('/test', function (request, response) {
 
 app.get('/test-list', function (req, res) {
   let data = [
-    {id: 1, name: 'name1', title: 'title1'}
-    , {id: 2, name: 'name2', title: 'title2'}
-    , {id: 3, name: 'name3', title: 'title3'}
-    , {id: 4, name: 'name', title: 'title4'}
+    {id: 1, name: 'name1', title: 'title1', tag: '用户 1'}
+    , {id: 2, name: 'name2', title: 'title2', tag: '用户 2'}
+    , {id: 3, name: 'name3', title: 'title3', tag: '用户 3'}
+    , {id: 4, name: 'name4', title: 'title4', tag: '用户 4'}
   ]
   res.send(data);
 });
